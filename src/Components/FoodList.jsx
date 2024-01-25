ad3f8512eaeff622d192066045e2e16893e80dc1
ritishbhardwaj <ritishbhardwaj02@gmail.com>
2024-01-25 00:41:27 -0800

    "Minor updates <typo update in FoodList.jsx> "

--- src/Components/FoodList.jsx
+++ src/Components/FoodList.jsx
@@ -2,45 +2,6 @@ import React from "react";
 import FoodItem from "./FoodItem";
 import Error from "./Error"
 
-// export default function FoodList(props) {
-//     let item = props.item;
-//   return (<>
-//     <div className='container pt-4'>
-//     <h3 className='pb-3'>Food Items</h3>
-//     <ul  className='list-group'>
-//         <li className='list-group-item'>{item[0]}</li>
-//         <li className='list-group-item'>{item[1]}</li>
-//         <li className='list-group-item'>{item[2]}</li>
-//         <li className='list-group-item'>{item[3]}</li>
-//     </ul>
-//     </div>
-
-//     </>
-//   )
-// }
-
-//    Another way
-// export default function FoodList({item}) {   //  here the {item <-- this name is same as the name in app.jsx}
-//     // let item = props.item;
-//   return (<>
-//     <div className='container pt-4'>
-//     <h3 className='pb-3'>Food Items</h3>
-//     <ul  className='list-group'>
-//         <li className='list-group-item'>{item[0]}</li>
-//         <li className='list-group-item'>{item[1]}</li>
-//         <li className='list-group-item'>{item[2]}</li>
-//         <li className='list-group-item'>{item[3]}</li>
-//         <li className='list-group-item'>{item[4]}</li>
-//         <li className='list-group-item'>{item[5]}</li>
-//     </ul>
-//     </div>
-
-//     </>
-//   )
-// }
-
-
-
 // Another way to do using mapping
 //here we Are using the looping concept using mapping
 
@@ -70,3 +31,46 @@ export default function FoodList({ item }) {
     }
   
 }
+
+
+//  First method told by sir
+
+// export default function FoodList(props) {
+    //     let item = props.item;
+//   return (<>
+//     <div className='container pt-4'>
+//     <h3 className='pb-3'>Food Items</h3>
+//     <ul  className='list-group'>
+//         <li className='list-group-item'>{item[0]}</li>
+//         <li className='list-group-item'>{item[1]}</li>
+//         <li className='list-group-item'>{item[2]}</li>
+//         <li className='list-group-item'>{item[3]}</li>
+//     </ul>
+//     </div>
+
+//     </>
+//   )
+// }
+
+//    Another way  told by sir
+// export default function FoodList({item}) {   //  here the {item <-- this name is same as the name in app.jsx}
+//     // let item = props.item;
+//   return (<>
+//     <div className='container pt-4'>
+//     <h3 className='pb-3'>Food Items</h3>
+//     <ul  className='list-group'>
+//         <li className='list-group-item'>{item[0]}</li>
+//         <li className='list-group-item'>{item[1]}</li>
+//         <li className='list-group-item'>{item[2]}</li>
+//         <li className='list-group-item'>{item[3]}</li>
+//         <li className='list-group-item'>{item[4]}</li>
+//         <li className='list-group-item'>{item[5]}</li>
+//     </ul>
+//     </div>
+
+//     </>
+//   )
+// }
+
+
+
